9d5d4001fa72dd14c860f65fac1c1e6050791b31
serghei <serghei.gurgurov@gmail.com>
2023-09-24 16:59:50 +0200

    day3 part 1

---
+++ src/d3/index.ts
@@ -0,0 +1,57 @@
+/* 
+    ## Description;
+
+    we have N rucksacks
+    each rucksack has 2 compartments
+
+    each string in the list represents 1 rucksack,
+    the charactes inside the string represent the items
+
+    exactly 1 item il placed incorrectly
+    and is contained in both compartments
+    (it occurring twice in the string doesn't
+     mean it's contained in both compartments)
+    
+    each item (character) has an associated number (priority)
+
+    [a-z,A-Z]=>[1-26,27-52] ascii -> [97-122,65-90]
+
+    ## Goal
+
+    for each racksack find the misplaced item,
+    then sum up the priorities of all items.
+
+
+    ## Alg-Options
+    O(n^2) -> for each caracter of set1 check if presentin set 2 -> 10^2 -> 100
+
+*/
+
+import { readFileSync } from 'node:fs';
+const input = readFileSync('./input.txt').toString();
+
+const pipe = (...fn: Function[]) => (data: any) => { return fn.forEach((fn) => data = fn(data)), data };
+const map = (fn) => (arr: any[]) => arr.map(fn);
+const array_sum = (arr: number[]) => arr.reduce((accumulator, value) => { return accumulator + value }, 0)
+
+const getCharacterValue = (asciiCode: number) => asciiCode > 96 ? asciiCode - 96 : asciiCode - 38;
+const toAscii = (char: string) => char.charCodeAt(0);
+
+const splitInTwo = (arr: any[] | string) => { return [arr.slice(0, Math.floor(arr.length / 2)), arr.slice(Math.floor(arr.length / 2))] }
+const parse = (input: string) => input.split("\n").map(splitInTwo)
+const findMisplaced = ([a1, a2]: [string, string]) => {
+    for (const left of a1)
+        for (const right of a2)
+            if (left == right)
+                return right
+}
+
+const getResult = pipe(
+    parse,
+    map(findMisplaced),
+    map(toAscii),
+    map(getCharacterValue),
+    array_sum
+)
+
+console.log("part one:", getResult(input))
